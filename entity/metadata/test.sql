329a7fc1e62028dcf53f4f343cb9dc60deb2823d
Abhishek <abhi20aug@gmail.com>
2018-06-06 13:23:35 +0530

    cleanup

--- entity/metadata/test.sql
+++
@@ -1,8 +0,0 @@
-create table Product_Schema (
-	Product_taxonomies VARCHAR(50),
-	Product VARCHAR(50),
-	Product_Variant VARCHAR(50),
-	Product_Attribute VARCHAR(50)
-);
-
-
--- plsql/examples/04_procedures.sql
+++ plsql/examples/04_procedures.sql
@@ -24,3 +24,17 @@ SELECT :session_var FROM dual;
 -- [Hello World.]
 
 
+The power to control the session rests with three commands. They were once called
+transaction control language (TCL) commands. Some documentation now speaks of them as data
+control language (DCL) commands. The book uses DCL to represent these three commands. The
+problem is trying to disambiguate this group of commands from Berkeley’s Tcl. 
+The commands are
+
+The COMMIT statement Commits all DML changes made from the beginning of the
+session or since the last ROLLBACK statement.
+
+The SAVEPOINT statement Divides two epochs. An epoch is defined by the
+transactions between two relative points of time. A SAVEPOINT delimits two epochs.
+
+The ROLLBACK statement Undoes all changes from now to an epoch or named
+SAVEPOINT, or now to the beginning of a SQL*Plus session.
