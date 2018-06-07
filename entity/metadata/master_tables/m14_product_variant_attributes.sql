0e0c65d06056dc83d406bc66298f9cf0085e0748
Abhishek <abhi20aug@gmail.com>
2018-06-07 13:58:40 +0530

    1.1

--- entity/metadata/master_tables/m14_product_variant_attributes.sql
+++ entity/metadata/master_tables/m14_product_variant_attributes.sql
@@ -1,10 +1,7 @@
 
-create table productsAttribute (
+create table product_variant_attributes (
 Product_Quality VARCHAR(9),
 Product_Colour VARCHAR(50),
-
 Product_Size VARCHAR(10),
-
 Product_ID INT
-
 );
