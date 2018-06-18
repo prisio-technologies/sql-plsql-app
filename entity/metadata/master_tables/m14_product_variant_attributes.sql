8cfd820589242629861308c4d39c7e3a92767d24
Abhishek <abhi20aug@gmail.com>
2018-06-18 18:35:38 +0530

    18/06

--- entity/metadata/master_tables/m14_product_variant_attributes.sql
+++
@@ -1,7 +0,0 @@
-drop table product_variant_attributes;
-create table product_variant_attributes (
-Product_Quality VARCHAR(9),
-Product_Colour VARCHAR(50),
-Product_Size VARCHAR(10),
-Product_ID INT
-);
